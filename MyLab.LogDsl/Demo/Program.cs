5b300fe9ef56ad41bc241d75948e0e0a29f443f2
Andrey O <ozzy.ext@gmail.com>
2018-09-04 22:15:36 +0300

    Add examples

---
+++ MyLab.LogDsl/Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyLab.LogDsl;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(c => c.AddConsole().SetMinimumLevel(LogLevel.Debug));
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var lf = serviceProvider.GetService<ILoggerFactory>();
+            var logger = lf.CreateLogger<Example>();
+
+            var example = new Example(logger);
+
+            example.Example1_SimpleAct();
+            example.Example2_SimpleDebug();
+            example.Example3_SimpleError();
+            example.Example4_ErrorWithException();
+            example.Example5_WithConditions();
+            example.Example6_WithMarkers();
+
+            Console.ReadKey(true);
+        }
+    }
+}
