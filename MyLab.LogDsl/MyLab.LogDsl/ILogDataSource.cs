00ec69c28546828723b74f9abc5b8ea7fe4e3b37
Andrey Osintsev <ozzy.ext@gmail.com>
2020-04-23 18:53:49 +0300

    Support dsl log initialize extencibility + Warning log

---
+++ MyLab.LogDsl/MyLab.LogDsl/ILogDataSource.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLab.LogDsl
+{
+    /// <summary>
+    /// Determines a log data source
+    /// </summary>
+    public interface ILogDataSource
+    {
+        /// <summary>
+        /// Adds log data to <see cref="DslLogEntityBuilder"/>
+        /// </summary>
+        void AddLogData(DslLogEntityBuilder builder);
+    }
+}
