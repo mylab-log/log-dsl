7167c4ff0e667d0b9b354a6dcd1cd83eac4f3b79
Andrey O <ozzy.ext@gmail.com>
2021-10-28 11:44:20 +0300

    Improve context injection tests

--- src/Tests/LogContextsInjectionBehavior.cs
+++ src/Tests/LogContextsInjectionBehavior.cs
@@ -10,7 +10,7 @@ namespace Tests
     public class LogContextsInjectionBehavior
     {
         [Fact]
-        public void ShouldApplyContext()
+        public void ShouldApplyContextForScope()
         {
             //Arrange
             var testCtxVal = Guid.NewGuid().ToString("N");
@@ -19,20 +19,47 @@ namespace Tests
             var lInstance = new TestLogger();
 
             var sp = new ServiceCollection()
-                .AddLogging(l => l
+                .AddLogging(lBuilder => lBuilder
                     .AddDsl()
                     .AddProvider(new TestLoggerProvider(lInstance)))
                 .AddDslLogContext<TestCtxApplier>()
-                .AddScoped(sp => testCtxSrv)
+                .AddScoped(s => testCtxSrv)
+                .AddScoped<TestTargetService>()
                 .BuildServiceProvider();
             var scope = sp.CreateScope();
-            IDslLogger l = scope.ServiceProvider.GetService<IDslLogger<LogContextsInjectionBehavior>>();
+            var targetService = scope.ServiceProvider.GetService<TestTargetService>();
 
             //Act
-            l.Error("foo").Write();
+            targetService.WriteError("foo");
 
             //Assert
-            Assert.Contains(lInstance.LastMessage.Labels, l => l.Key == testCtxVal && l.Value == "true");
+            Assert.Contains(lInstance.LastMessage.Labels, kv => kv.Key == testCtxVal && kv.Value == "true");
+        }
+
+        [Fact]
+        public void ShouldApplyContextWithoutScope()
+        {
+            //Arrange
+            var testCtxVal = Guid.NewGuid().ToString("N");
+            var testCtxSrv = new TestCtxService(testCtxVal);
+
+            var lInstance = new TestLogger();
+
+            var sp = new ServiceCollection()
+                .AddLogging(lBuilder => lBuilder
+                    .AddDsl()
+                    .AddProvider(new TestLoggerProvider(lInstance)))
+                .AddDslLogContext<TestCtxApplier>()
+                .AddSingleton(testCtxSrv)
+                .AddSingleton<TestTargetService>()
+                .BuildServiceProvider();
+            var targetService = sp.GetService<TestTargetService>();
+
+            //Act
+            targetService.WriteError("foo");
+
+            //Assert
+            Assert.Contains(lInstance.LastMessage.Labels, kv => kv.Key == testCtxVal && kv.Value == "true");
         }
 
         class TestCtxApplier : IDslLogContextApplier
@@ -57,6 +84,21 @@ namespace Tests
             public TestCtxService(string value)
             {
                 Value = value;
+            }
+        }
+
+        class TestTargetService
+        {
+            private readonly IDslLogger<TestTargetService> _logger;
+
+            public TestTargetService(IDslLogger<TestTargetService> logger)
+            {
+                _logger = logger;
+            }
+
+            public void WriteError(string msg)
+            {
+                _logger.Error(msg).Write();
             }
         }
     }
